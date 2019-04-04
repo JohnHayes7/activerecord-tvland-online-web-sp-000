604e9cafd290bb9e8b3a1221c4c705ea28eb2045
John Hayes <johnnyhayes7@gmail.com>
2019-04-04 14:42:45 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,11 +2,5 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
-  def build_network(call_letters)
-    
-  end
   
-  def network
-    
-  end
 end
